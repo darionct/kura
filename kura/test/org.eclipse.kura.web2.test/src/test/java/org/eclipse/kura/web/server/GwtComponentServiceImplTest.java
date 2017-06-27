afb7554d4fe3f9c10732d567c9566720b99d45f7
Dario Novakovic <dario.novakovic@comtrade.com>
2017-06-27 11:43:38 +0200

    test coverage improved
    Signed-off-by: Dario Novakovic <dario.novakovic@comtrade.com>

--- kura/test/org.eclipse.kura.web2.test/src/test/java/org/eclipse/kura/web/server/GwtComponentServiceImplTest.java
+++ kura/test/org.eclipse.kura.web2.test/src/test/java/org/eclipse/kura/web/server/GwtComponentServiceImplTest.java
@@ -3,8 +3,6 @@ package org.eclipse.kura.web.server;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -12,6 +10,7 @@ import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -21,12 +20,15 @@ import org.eclipse.kura.configuration.ComponentConfiguration;
 import org.eclipse.kura.configuration.ConfigurableComponent;
 import org.eclipse.kura.configuration.ConfigurationService;
 import org.eclipse.kura.core.configuration.ComponentConfigurationImpl;
+import org.eclipse.kura.core.configuration.metatype.Tad;
 import org.eclipse.kura.core.configuration.metatype.Tocd;
+import org.eclipse.kura.core.configuration.metatype.Tscalar;
 import org.eclipse.kura.core.testutil.TestUtil;
 import org.eclipse.kura.web.Console;
 import org.eclipse.kura.web.shared.GwtKuraException;
 import org.eclipse.kura.web.shared.model.GwtConfigComponent;
 import org.eclipse.kura.web.shared.model.GwtConfigParameter;
+import org.eclipse.kura.web.shared.model.GwtConfigParameter.GwtConfigParameterType;
 import org.eclipse.kura.web.shared.model.GwtXSRFToken;
 import org.eclipse.kura.wire.WireHelperService;
 import org.junit.After;
@@ -51,17 +53,20 @@ public class GwtComponentServiceImplTest {
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
-        HashMap compCfgProperties = new HashMap();
-        // compCfgProperties.
-        ComponentConfigurationImpl compCfg = new ComponentConfigurationImpl("testPid", new Tocd(), compCfgProperties);
+        ComponentConfigurationImpl compCfg1 = new ComponentConfigurationImpl("testPid", createTocd("tadId"),
+                createCompCfgProperies());
+        ComponentConfigurationImpl compCfg2 = new ComponentConfigurationImpl("zPid", createTocd("tadId2"),
+                createCompCfgProperies());
         List<ComponentConfiguration> compCfgList = new ArrayList<>();
-        compCfgList.add(compCfg);
+        compCfgList.add(compCfg1);
+        compCfgList.add(compCfg2);
 
         configurableComponentSvc = mock(ConfigurableComponent.class);
         ServiceReference<ConfigurableComponent> svcRef = mock(ServiceReference.class);
 
         configurationSvc = mock(ConfigurationService.class);
-        when(configurationSvc.getComponentConfiguration("testPid")).thenReturn(compCfg);
+        when(configurationSvc.getComponentConfiguration("testPid")).thenReturn(compCfg1);
+        when(configurationSvc.getDefaultComponentConfiguration("factoryPid")).thenReturn(compCfg2);
         when(configurationSvc.getComponentConfigurations()).thenReturn(compCfgList);
         ServiceReference<ConfigurationService> svcRefConfigSvc = mock(ServiceReference.class);
 
@@ -167,8 +172,13 @@ public class GwtComponentServiceImplTest {
     @Test
     public void testUpdateComponentConfiguration() throws GwtKuraException, KuraException {
         GwtConfigParameter param = new GwtConfigParameter();
-        param.setMax("0");
-        param.setMin("100");
+        param.setId("id");
+        param.setMax("100");
+        param.setMin("1");
+        param.setCardinality(1);
+        param.setDefault("50");
+        param.setValue("7");
+        param.setType(GwtConfigParameterType.INTEGER);
         List<GwtConfigParameter> parameters = new ArrayList<>();
         parameters.add(param);
         GwtConfigComponent gwtCompConfig = new GwtConfigComponent();
@@ -176,7 +186,9 @@ public class GwtComponentServiceImplTest {
         gwtCompConfig.setComponentId("testPid");
         gwtCompConfig.setParameters(parameters);
         gwtCompService.updateComponentConfiguration(xsrfToken, gwtCompConfig);
-        verify(configurationSvc).updateConfiguration(eq("testPid"), any());
+        Map<String, Object> expected = new HashMap<>();
+        expected.put("id", 7);
+        verify(configurationSvc).updateConfiguration("testPid", expected);
     }
 
     @Test
@@ -192,11 +204,6 @@ public class GwtComponentServiceImplTest {
     }
 
     @Test
-    public void testFindFactoryComponents() throws GwtKuraException {
-        // gwtCompService.findFactoryComponents(xsrfToken); // TODO
-    }
-
-    @Test
     public void testFindWireComponentConfigurationFromPid() throws GwtKuraException {
         GwtConfigComponent wireConfigComp = gwtCompService.findWireComponentConfigurationFromPid(xsrfToken, "testPid",
                 "factoryPid", new HashMap<>());
@@ -204,9 +211,33 @@ public class GwtComponentServiceImplTest {
     }
 
     @Test
+    public void testFindDeletedWireComponentConfigurationFromPid() throws GwtKuraException {
+        GwtConfigComponent wireConfigComp = gwtCompService.findWireComponentConfigurationFromPid(xsrfToken,
+                "unknownPid", "factoryPid", new HashMap<>());
+        assertNotNull(wireConfigComp);
+    }
+
+    @Test
     public void testUpdateProperties() throws GwtKuraException {
         boolean result = gwtCompService.updateProperties(xsrfToken, "testPid", new HashMap<>());
         assertTrue(result);
+    }
+
+    // helper methods
+    private static Tocd createTocd(String id) {
+        Tad tad = new Tad();
+        tad.setId(id);
+        tad.setType(Tscalar.STRING);
+        Tocd tocd = new Tocd();
+        tocd.addAD(tad);
+        return tocd;
+    }
+
+    private static HashMap<String, Object> createCompCfgProperies() {
+        HashMap<String, Object> compCfgProperties = new HashMap<>();
+        compCfgProperties.put("tadId", "value");
+        compCfgProperties.put("values", new String[] { "value", "value2" });
+        return compCfgProperties;
     }
 
 }
